1b463e8a19a4bbb6193b740bad38e1cc659c185c
Cocodilla <alexakitten@yandex.com>
2023-03-26 21:40:00 +0300

    Shared prefs load save

--- app/src/main/java/com/example/basememory/MainActivity.java
+++ app/src/main/java/com/example/basememory/MainActivity.java
@@ -2,13 +2,65 @@ package com.example.basememory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View .OnClickListener{
+
+    EditText et;
+    Button btnSafe, btnLoad;
+    SharedPreferences sharedPreferences;
+    final String SAVED_TEXT = "saved Text";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et = (EditText) findViewById(R.id.etText);
+        btnSafe = (Button) findViewById(R.id.btnSafe);
+        btnLoad = (Button) findViewById(R.id.btnLoad);
+
+btnSafe.setOnClickListener(this);
+btnLoad.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.btnSafe:
+                saveText();
+                break;
+            case R.id.btnLoad:
+                loadText();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void loadText() {
+
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+        String savedText = sharedPreferences.getString(SAVED_TEXT,"Nothing was saved");
+        et.setText(savedText);
+        Toast.makeText(this, "The text was loaded", Toast.LENGTH_LONG).show();
+
+    }
+
+    private void saveText() {
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor ed = sharedPreferences.edit();
+        ed.putString(SAVED_TEXT, et.getText().toString());
+        ed.commit();
+        Toast.makeText(this, "The text was saved", Toast.LENGTH_LONG).show();
+
+
     }
 }
